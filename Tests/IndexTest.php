6145c4b4c3d974199fa7d1823cdeefb8037f83c0
student <student.ksipt.ru>
2025-10-24 15:58:18 +0300

    контрольная номер 1

--- Tests/IndexTest.php
+++ Tests/IndexTest.php
@@ -1,78 +1,48 @@
 <?php
-//В массиве хранятся следующие данные о товарах интернет магазина:
-//наименование, цена, количество на складе.
-//Вывести на экран сводную информацию о товарах в виде массива с данными: количество наименований товара,
-//самый дешевый и самый дорогой товар, общее количество всех товаров на складе и общая стоимость всех товаров.
-//Напишите функцию storage($arr), принимающую параметром массив и
-//возращающую массив со сводной информацией.
-//Входной массив создайте по образцу.
-/* пример входного массива
-$arr = [
-  [
-    'title' => 'Altair',
-    'price' => 15000,
-    'quantity' => 15,
-  ]
-];
-Результ работы функции должен быть с такими полями (образец)
-[
- 'quantityCards' => 10,
- 'maxPrice' => 20000,
- 'minPrice' => 100,
- 'allQuantity' => 30,
- 'allSumm' => 75000
-]
-*/
-function storage($arr) {
-    $result= [
-        'quantityCards' => count($arr),
-        'maxPrice' => 0,
-        'minPrice' => PHP_INT_MAX,
-        'allQuantity' => 0,
-        'allSum' => 0
-    ];
+namespace Tests;
+use PHPUnit\Framework\TestCase;
+require 'Задание 1\index.php';
+require 'Задание 2\index.php';
 
-    foreach ($arr as $product) {
-        if ($product['price'] > $result['maxPrice']) {
-            $result['maxPrice'] = $product['price'];
-        }
-        if ($product['price'] < $result['minPrice']) {
-            $result['minPrice']  = $product['price'];
-        }
-        $result['allQuantity'] += $product['quantity'];
-        $result['allSum'] += $product['price']  * $product['quantity'];
+
+class IndexTest extends TestCase
+{
+    public function testIndex1(): void
+    {
+     $str1 = "H1re Krishna Hare a вв kkd жжды :-O";
+     $str2 = "H Krishna Kdna Krishna Krishna Hare Hare :-O";
+       
+       $result = countEqualWord($str1,$str2);
+       self::assertEquals( ["Krishna", "Hare",":-O"],$result);
     }
-    return $result;
+    public function testIndex2(): void
+    {
+      $arr = [
+          [
+            'title' => 'Altair',
+            'price' => 15000,
+            'quantity' => 15,
+          ],[
+          'title' => 'Stels Focus',
+          'price' => 11000,
+          'quantity' => 5,
+          ],[
+          'title' => 'STARK Madness',
+          'price' => 25000,
+          'quantity' => 4,
+          ],[
+          'title' => 'FORWARD AZURE',
+          'price' => 8000,
+          'quantity' => 8,
+        ]
+        ];
+       
+       $result = storage($arr);
+       self::assertEquals(['quantityCards' => 4,
+          'maxPrice' => 25000,
+          'minPrice' => 8000,
+          'allQuantity' => 32,
+          'allSumm' => 444000],$result);
+    }
 }
-$arr = [
-      [
-        'title' => 'Altair',
-        'price' => 15000,
-        'quantity' => 15,
-      ],[
-      'title' => 'Stels',
-      'price' => 11000,
-      'quantity' => 5,
-      ],[
-      'title' => 'Stark',
-      'price' => 25000,
-      'quantity' => 4,
-      ],[
-      'title' => 'FORWARD ',
-      'price' => 8000,
-      'quantity' => 8,
-    ]
-];
-$result = storage($arr);
 
-echo "Количество наименований: " .
-$result['quantityCards'] . "<br>";
-echo "Самый дорогой товар: " .
-$result['maxPrice'] . "<br>";
-echo "Самый дешевый товар: " .
-$result['minPrice'] . "<br>";
-echo "Общее количество товаров: " .
-$result['allQuantity'] . "<br>";
-echo "Общая стоимость: " .
-$result['allSum'] . "<br>";
-?>
--- Задание 2/index.php
+++ Задание 2/index.php
@@ -1,68 +1,78 @@
 <?php
+//В массиве хранятся следующие данные о товарах интернет магазина:
+//наименование, цена, количество на складе.
+//Вывести на экран сводную информацию о товарах в виде массива с данными: количество наименований товара,
+//самый дешевый и самый дорогой товар, общее количество всех товаров на складе и общая стоимость всех товаров.
+//Напишите функцию storage($arr), принимающую параметром массив и
+//возращающую массив со сводной информацией.
+//Входной массив создайте по образцу.
+/* пример входного массива
+$arr = [
+  [
+    'title' => 'Altair',
+    'price' => 15000,
+    'quantity' => 15,
+  ]
+];
+Результ работы функции должен быть с такими полями (образец)
+[
+ 'quantityCards' => 10,
+ 'maxPrice' => 20000,
+ 'minPrice' => 100,
+ 'allQuantity' => 30,
+ 'allSumm' => 75000
+]
+*/
 function storage($arr) {
-    if (empty($arr)) return ['quantityCards'=>0, 'maxPrice'=>0, 'minPrice'=>0, 'allQuantity'=>0, 'allSumm'=>0];
-    
-    $quantityCards = count($arr);
-    $maxPrice = $arr[0]['price'];
-    $minPrice = $arr[0]['price'];
-    $allQuantity = 0;
-    $allSumm = 0;
-    
-    foreach ($arr as $product) {
-        if ($product['price'] > $maxPrice) $maxPrice = $product['price'];
-        if ($product['price'] < $minPrice) $minPrice = $product['price'];
-        $allQuantity += $product['quantity'];
-        $allSumm += $product['price'] * $product['quantity'];
-    }
-    
-    return [
-        'quantityCards' => $quantityCards,
-        'maxPrice' => $maxPrice,
-        'minPrice' => $minPrice,
-        'allQuantity' => $allQuantity,
-        'allSumm' => $allSumm
+    $result= [
+        'quantityCards' => count($arr),
+        'maxPrice' => 0,
+        'minPrice' => 0,
+        'allQuantity' => 0,
+        'allSum' => 0
     ];
-}
 
-if ($_POST) {
-    $products = [];
-    for ($i = 0; $i < 3; $i++) {
-        if ($_POST["title_$i"]) {
-            $products[] = [
-                'title' => $_POST["title_$i"],
-                'price' => (float)$_POST["price_$i"],
-                'quantity' => (int)$_POST["quantity_$i"]
-            ];
+    foreach ($arr as $product) {
+        if ($product['price'] > $result['maxPrice']) {
+            $result['maxPrice'] = $product['price'];
         }
+        if ($product['price'] < $result['minPrice']) {
+            $result['minPrice']  = $product['price'];
+        }
+        $result['allQuantity'] += $product['quantity'];
+        $result['allSum'] += $product['price']  * $product['quantity'];
     }
-    $result = storage($products);
+    return $result;
 }
+$arr = [
+      [
+        'title' => 'Altair',
+        'price' => 15000,
+        'quantity' => 15,
+      ],[
+      'title' => 'Stels',
+      'price' => 11000,
+      'quantity' => 5,
+      ],[
+      'title' => 'Stark',
+      'price' => 25000,
+      'quantity' => 4,
+      ],[
+      'title' => 'FORWARD ',
+      'price' => 8000,
+      'quantity' => 8,
+    ]
+];
+$result = storage($arr);
+
+echo "Количество наименований: " .
+$result['quantityCards'] . "<br>";
+echo "Самый дорогой товар: " .
+$result['maxPrice'] . "<br>";
+echo "Самый дешевый товар: " .
+$result['minPrice'] . "<br>";
+echo "Общее количество товаров: " .
+$result['allQuantity'] . "<br>";
+echo "Общая стоимость: " .
+$result['allSum'] . "<br>";
 ?>
-
-<form method="post">
-    <h3>Товар 1</h3>
-    Название: <input type="text" name="title_0"><br>
-    Цена: <input type="number" name="price_0"><br>
-    Количество: <input type="number" name="quantity_0"><br>
-
-    <h3>Товар 2</h3>
-    Название: <input type="text" name="title_1"><br>
-    Цена: <input type="number" name="price_1"><br>
-    Количество: <input type="number" name="quantity_1"><br>
-
-    <h3>Товар 3</h3>
-    Название: <input type="text" name="title_2"><br>
-    Цена: <input type="number" name="price_2"><br>
-    Количество: <input type="number" name="quantity_2"><br>
-
-    <input type="submit" value="Анализировать">
-</form>
-
-<?php if (isset($result)): ?>
-    <h2>Результат:</h2>
-    Количество наименований: <?=$result['quantityCards']?><br>
-    Максимальная цена: <?=$result['maxPrice']?><br>
-    Минимальная цена: <?=$result['minPrice']?><br>
-    Общее количество: <?=$result['allQuantity']?><br>
-    Общая стоимость: <?=$result['allSumm']?><br>
-<?php endif; ?>
